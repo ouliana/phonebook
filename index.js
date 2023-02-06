40e36de73992fb1a2c39d4ead6cdfa9c82c0f9d8
ouliana <ouliana.kotik@gmail.com>
2023-02-06 20:16:43 +0300

    minor changes

--- index.js
+++ index.js
@@ -4,44 +4,43 @@ app.use(express.json());
 
 const PORT = 3001;
 
- 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
-
+});
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
 
 app.get('/info', (request, response) => {
-  const info = `Phonebook has info for ${persons.length} people<br>${new Date()}`;
+  const info = `Phonebook has info for ${
+    persons.length
+  } people<br>${new Date()}`;
   response.send(info);
-})
+});
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
-})
+});
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
@@ -55,33 +54,35 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
-   
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => { 
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
 
   persons = persons.filter(p => p.id !== id);
 
   response.status(204).end();
-})
+});
 
 const generateId = () => {
-  let id = Math.floor(Math.random() * (900) + 100);
+  let min = 100;
+  let max = 1000;
+  let id = Math.floor(Math.random() * (max - min)) + min;
   while (persons.find(p => p.id === id)) {
-    id = Math.floor(Math.random() * (900) + 100);
+    id = Math.floor(Math.random() * (max - min)) + min;
   }
   return id;
-}
+};
 
 const handleError = ({ name, number }) => {
   if (!name) return { error: 'no name received' };
   if (!number) return { error: 'no number received' };
-  if (persons.find(p => p.name === name)) return {
-    error: 'name must be unique'
-  }
+  if (persons.find(p => p.name === name))
+    return {
+      error: 'name must be unique',
+    };
   return null;
-}
+};
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -89,21 +90,18 @@ app.post('/api/persons', (request, response) => {
   let error = handleError(body);
 
   if (error) {
-      response.status(400).send(error)
+    response.status(400).send(error);
   } else {
     const id = generateId();
-  
-  const person = {
-    id: id,
-    name: body.name,
-    number: body.number
+
+    const person = {
+      id: id,
+      name: body.name,
+      number: body.number,
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
   }
-  
-  persons = persons.concat(person);
-
-  response.json(person);
-  }
-
-})
-
-
+});
